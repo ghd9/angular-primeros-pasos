b5f09d3d639d619b57e632ac55d1923c858481c0
Guillermo de las Heras Diaz <guillermodelasherasdiaz@iMac-de-Guillermo.local>
2023-07-17 17:37:30 +0200

    Fin de sesión 4

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html'
+})
+export class CounterComponent {
+
+  public title: String = 'Hola mundo';
+  public counter: number=10;
+
+  increaseBy(value:number):void{
+    this.counter += value;
+  }
+
+  reset():void{
+    this.counter =10;
+  }
+
+}
